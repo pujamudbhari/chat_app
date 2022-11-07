a755aa9ebb9506124b2979b535f404b98e33430b
pujamudbhari <pujamudbhari1234@gmail.com>
2022-11-07 19:28:21 +0545

    message_display_done

--- chat_app/chat/models.py
+++ chat_app/chat/models.py
@@ -1,17 +1,11 @@
-from datetime import datetime
-from email.policy import default
-from pyexpat import model
-from unittest.util import _MAX_LENGTH
 from django.db import models
-
-# Create your models here.
+from datetime import datetime
 
 class Room(models.Model):
     name = models.CharField(max_length=1000)
 
 class Message(models.Model):
-    name = models.CharField(max_length=1000000)
-    date = models.DateField(default=datetime.now, blank=True)
-    room= models.CharField(max_length= 1000000)
-    user= models.CharField(max_length= 10000000)
-
+    value = models.CharField(max_length=10000000)
+    date = models.DateTimeField(default=datetime.now, blank=True)
+    room = models.CharField(max_length=1000000)
+    user = models.CharField(max_length=1000000)
